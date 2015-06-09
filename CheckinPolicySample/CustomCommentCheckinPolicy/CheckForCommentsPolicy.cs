8c1ea5af2244a31e08b1ee549322a189001fb7fd
Maik Hanns <maikhanns@web.de>
2015-06-09 21:55:11 +0200

    add CheckinPolicy Sample

---
+++ CheckinPolicySample/CustomCommentCheckinPolicy/CheckForCommentsPolicy.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.TeamFoundation.VersionControl.Client;
+
+namespace CustomCommentCheckinPolicy
+{
+    [Serializable]
+    public class CheckForCommentsPolicy : PolicyBase
+    {
+        /// <summary>
+        /// Shows a configuration dialog box to the user to specify options for the check-in policy.
+        /// </summary>
+        /// <returns>
+        /// True if the dialog box opened; otherwise, false.
+        /// </returns>
+        /// <param name="policyEditArgs">Arguments for the configuration dialog box.</param>
+        public override bool Edit(IPolicyEditArgs policyEditArgs)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the name of this policy.
+        /// </summary>
+        /// <returns>
+        /// The name of this policy.
+        /// </returns>
+        public override string Type
+        {
+            get { return "Custom Comment Policy"; }
+        }
+
+        /// <summary>
+        /// Gets the description of this kind of policy.
+        /// </summary>
+        /// <returns>
+        /// The description of this kind of policy.
+        /// </returns>
+        public override string TypeDescription
+        {
+            get { return "This policy will check the comment for 10 or more characters."; }
+        }
+
+        /// <summary>
+        /// Gets the description of this policy.
+        /// </summary>
+        /// <returns>
+        /// The description of this policy.
+        /// </returns>
+        public override string Description
+        {
+            get { return "The Checkin Comment must contain 10 or more characters"; }
+        }
+
+        /// <summary>
+        /// Called if the user double-clicks on a policy failure.
+        /// </summary>
+        /// <param name="failure">The policy failure that causes this event.</param>
+        public override void Activate(PolicyFailure failure)
+        {
+            MessageBox.Show("Please type a comment with 10 or more characters", "Resolving Comment Failure");
+        }
+
+        /// <summary>
+        /// Gets a flag that describes whether this policy is configurable.
+        /// </summary>
+        /// <returns>
+        /// True if this policy is configurable; otherwise, false.
+        /// </returns>
+        public override bool CanEdit
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Performs the policy evaluation.
+        /// </summary>
+        /// <returns>
+        /// An array of PolicyFailures that results from the evaluation.
+        /// </returns>
+        public override PolicyFailure[] Evaluate()
+        {
+            var comment = PendingCheckin.PendingChanges.Comment.Trim();
+
+            if (comment.Length < 10)
+                return new[]
+                {
+                    new PolicyFailure("Please type a commen with 10 or more characters", this)
+                };
+
+            return new PolicyFailure[0];
+        }
+    }
+}
---
+++ CheckinPolicySample/CustomCommentCheckinPolicy/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CustomCheckInPolicy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CustomCheckInPolicy")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("098fabac-2d5e-43be-9fac-f02388dc2589")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
